cac5d46f6e80d4d7d5d2c638aa8db86c46d6d1ab
Suraj <surajb92@gmail.com>
2025-09-13 14:52:03 +0530

    Implemented multiple rooms & gating guests to just one room (security throgh js still needs work though).

--- main-app.py
+++ main-app.py
@@ -6,9 +6,12 @@ from sys import exit
 from string import ascii_letters
 from dotenv import load_dotenv, find_dotenv
 
-from flask import Flask, flash, jsonify, render_template, redirect, request, session, url_for
+from flask import Flask, flash, jsonify, make_response, render_template, redirect, request, session, url_for
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, ValidationError
@@ -17,7 +20,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+socketio = SocketIO(app, manage_session=False)
 
 if load_dotenv():
     # POSTGRESQL Database configuration
@@ -25,6 +30,7 @@ if load_dotenv():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
+    ms = Marshmallow(app)
 else:
     exit("Error: Local environment variables have not been set up properly. Check github manual.")
 
@@ -35,7 +41,13 @@ class chat_msg(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender = db.Column(db.String(100), nullable=False)
     message = db.Column(db.String(1000), nullable=False)
+    room = db.Column(db.String(100), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class chatSchema(ms.SQLAlchemyAutoSchema):
+    class Meta:
+        model = chat_msg
+        load_instance = True
 
 class user_list(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -73,7 +85,6 @@ def checkdb():
 def home():
     uname = session.get('uname')
     utype = session.get('utype')
-    session['room'] = "room0"
     lgn = login_form()
     if request.method == "POST":
         if 'room-btn' in request.form:
@@ -82,22 +93,19 @@ def home():
             session['uname'] = lgn.username.data
             session['utype'] = 'registered'
             return redirect(url_for('home'))
-        else:
-            return render_template('index.html',user=uname,usertype=utype,loginform=lgn)
-    else:
-        if not uname:
-            while True:
-                r = [random.choice(ascii_letters) for _ in range(5)]
-                rs = ''.join(r)
-                gname = 'guest-'+rs
-                print(gname)
-                if gname not in guests:
-                    uname = gname
-                    session['uname'] = uname
-                    session['utype'] = 'guest'
-                    utype = 'guest'
-                    break
-        return render_template('index.html',user=uname,usertype=utype,loginform=lgn)
+    if not uname:
+        while True:
+            r = [random.choice(ascii_letters) for _ in range(5)]
+            rs = ''.join(r)
+            gname = 'guest-'+rs
+            if gname not in guests:
+                uname = gname
+                session['uname'] = uname
+                session['utype'] = 'guest'
+                session['room'] = 'general'
+                utype = 'guest'
+                break
+    return render_template('index.html',user=uname,usertype=utype,loginform=lgn)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -121,19 +129,37 @@ def logout():
 
 @app.route('/room', methods=["GET", "POST"])
 def chatroom():
-    room = session.get('room')
+    CHAT_LIMIT = 70
     uname = session.get('uname')
     utype = session.get('utype')
-    lgn = login_form()
     if not uname:
         return redirect(url_for('home'))
-    chatlog = chat_msg.query.all()
+    lgn = login_form()
+    room = session.get('room')
+    history = session.get('history')
+    if not history:
+        history = 50
+        session['history'] = 50
+    elif history > CHAT_LIMIT:
+        history = CHAT_LIMIT
+        session['history'] = CHAT_LIMIT
+    #c = chat_msg.query.filter_by(room=room).order_by(chat_msg.id.desc()).all()
+    c = chat_msg.query.filter_by(room=room).order_by(chat_msg.id.desc()).limit(history).all()
+    chschema = chatSchema(many=True)
+    chatlog = chschema.dump(c)
     if request.method == "POST":
         if lgn.validate_on_submit():
             session['uname'] = lgn.username.data
             session['utype'] = 'registered'
             return redirect(url_for('chatroom'))
     return render_template("chatpage.html",user=uname,usertype=utype,room=room,chats=chatlog,loginform=lgn)
+
+@app.route('/api/getchat')
+def changeroom():
+    c = chat_msg.query.filter_by(room=session['room']).order_by(chat_msg.id.desc()).limit(session['history']).all()
+    chschema = chatSchema(many=True)
+    chatlog = chschema.dump(c)
+    return jsonify(chatlog)
 
 # ------------------------------
 #   Socket handlers
@@ -146,17 +172,14 @@ def handle_connect():
     if uname is None or room is None:
         return
     join_room(room)
-    emit('sendmsg',
-        {   "sender": "",
-            "message": f"{uname} has entered the chat"
-        },
-        to=room
-    )
+    # emit() # push online members from here
     # rooms[room]["members"] += 1
 
-@socketio.on('login')
-def handle_login():
-    pass
+@socketio.on('changeroom')
+def handle_changeroom(payload):
+    newroom = payload["newroom"]
+    join_room(newroom)
+    session['room'] = newroom
 
 @socketio.on('message')
 def handle_message(payload):
@@ -167,7 +190,8 @@ def handle_message(payload):
         "message": payload["message"]
     }
     emit('sendmsg', msg, to=room)
-    newmsg = chat_msg(sender=uname,message=payload["message"],timestamp=datetime.now())
+    newmsg = chat_msg(sender=uname,message=payload["message"],timestamp=datetime.now(),room=room)
+    session['history'] += 1
     db.session.add(newmsg)
     db.session.commit()
 
--- static/js/chat_handler.js
+++ static/js/chat_handler.js
@@ -1,5 +1,13 @@
 var socketio = io();
 socketio.on("sendmsg", function (message) { createChatItem(message.message, message.sender) });
+//socketio.on("roomchg", function (message) { createChatItem(message.message, message.sender) });
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    // Populate chat here?
+    var chat_list = document.getElementById(room);
+    chat_list.style.display = "block";
+    populateChat(init_chat);
+});
 
 document.addEventListener('keydown', function (event) {
     if (event.key === "Enter" && document.getElementById('userchat') === document.activeElement) {
@@ -7,17 +15,52 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    chat_list = document.getElementById('chat-list');
+function populateChat(chats) {
+    var chat_list = document.getElementById(room);
+    for (let c in chats) {
+        t = createChatElement(chats[c]);
+        chat_list.prepend(t);
+    }
+    // This auto scrolls to front, can add functionality later if needed
     chat_list.scrollTop = chat_list.scrollHeight;
-});
+}
+
+function createChatElement(chat) {
+    var sent_by_me = uname === chat.sender;
+    
+    const newchat = document.createElement('div');
+    newchat.className = 'message-item '
+    newchat.className += sent_by_me ? 'self-message-item' : 'peer-message-item';
+    const sndr = document.createElement('small');
+    sndr.className = sent_by_me ? 'user-text' : 'peer-text';
+    sndr.textContent = chat.sender;
+    const msg = document.createElement('div');
+    msg.textContent = chat.message;
+    const ts = document.createElement('small');
+    ts.className = sent_by_me ? 'muted-text' : 'muted-text-white';
+    ts.textContent = new Date(chat.timestamp).toLocaleString('en-GB', {hour12: true}).replace(/am/i, "AM").replace(/pm/i, "PM");
+
+    newchat.appendChild(sndr);
+    newchat.appendChild(msg);
+    newchat.appendChild(ts);
+
+    return newchat;
+
+    /*
+    var content = `
+    <div class="message-item ${sent_by_me ? "self-message-item" : "peer-message-item"}">
+    <small class="${sent_by_me ? "user-text" : "peer-text"}">${sender}</small>
+    <div>${message}</div>
+    <small class="${sent_by_me ? "muted-text" : "muted-text-white"}">${new Date().toLocaleString('en-GB', {hour12: true}).replace(/am/i, "AM").replace(/pm/i, "PM")}</small>
+    </div>`
+    return content;
+    */
+}
 
 function createChatItem(message, sender) {
-    var chat_list = document.getElementById("chat-list");
+    var chat_list = document.getElementById(room);
     var usersend = uname === sender;
-    if (sender === "") {
-        content = `<p class="member-activity">${message}</p>`;
-    } else {
+    if (sender !== "") {
         var content = `
         <div class="message-item ${usersend ? "self-message-item" : "peer-message-item"}">
         <small class="${usersend ? "user-text" : "peer-text"}">${sender}</small>
@@ -36,4 +79,29 @@ function sendMessage() {
     var msg = userchat.value;
     socketio.emit("message", { message: msg });
     userchat.value = "";
+}
+
+function changeRoom(roomid) {
+    if (room == roomid) {
+        return;
+    }
+    var old_chat = document.getElementById(room);
+    var new_chat = document.getElementById(roomid);
+    old_chat.style.display = "none";
+    new_chat.style.display = "block";
+    socketio.emit("changeroom", { newroom: roomid} );
+    fetch('/api/getchat')
+        .then(response=> {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(newchats => {
+            room = roomid;
+            populateChat(newchats);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
